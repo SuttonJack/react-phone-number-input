22c1f46aa55bd879984d1dd2fa5e21e010df9c6b
catamphetamine <kuchumovn@gmail.com>
2022-03-28 21:27:11 +0300

    #125. TypeScript fix on `react-hook-form` components

--- react-hook-form-input/index.d.ts
+++ react-hook-form-input/index.d.ts
@@ -19,12 +19,12 @@ export {
 
 // `Props` are used in:
 // * `react-hook-form-input-core/index.d.ts`
-export type Props<InputComponentProps> = BaseProps<InputComponentProps> & ReactHookFormComponentProps & {
+export type Props<InputComponentProps, FormValues> = BaseProps<InputComponentProps> & ReactHookFormComponentProps<FormValues> & {
   // onChange?(event: React.ChangeEvent<HTMLInputElement>): void;
   // onBlur?(event: React.FocusEvent<HTMLInputElement>): void;
 }
 
-type PhoneInputType<InputComponentProps = DefaultInputComponentProps> = (props: Props<InputComponentProps>) => JSX.Element;
+type PhoneInputType<InputComponentProps = DefaultInputComponentProps, FormValues> = (props: Props<InputComponentProps, FormValues>) => JSX.Element;
 
 declare const PhoneInput: PhoneInputType;
 
